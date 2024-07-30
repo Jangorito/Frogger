b4fbe6c74afa6f69396051826da3710fe0a0755d
Jango <psyea6@nottingham.ac.uk>
2024-07-30 19:42:41 +0100

    Initial commit

---
+++ src/main/java/org/example/froggergame/BackgroundImage.java
@@ -0,0 +1,18 @@
+package org.example.froggergame;
+
+import javafx.scene.image.Image;
+
+public class BackgroundImage extends Actor{
+
+	@Override
+	public void act(long now) {
+		
+		
+	}
+	
+	public BackgroundImage(String imageLink) {
+		setImage(new Image(imageLink, 600, 800, true, true));
+		
+	}
+
+}
